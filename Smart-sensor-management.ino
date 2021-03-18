11a4467ffdc474e1bb5de6362c6e03019d98ca49
Lucas Hijman <0968128@hr.nl>
2021-03-18 01:13:44 +0100

    code cleanup
    removed unnecessary lines

--- Smart-sensor-management.ino
+++ Smart-sensor-management.ino
@@ -2,7 +2,7 @@
 #include "Light.h"
 #include "Button1.h"
 
-bool leftButtonPressed, rightButtonPressed;
+bool rightButtonPressed;
 int neoPixel = 0;
 Light* light = new Light(neoPixel);
 Button1* button_1 = new Button1();
@@ -17,10 +17,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   int lightStrength = CircuitPlayground.lightSensor();
   int colorThroughLightStrength = lightStrength * 0.24926686217;
-  Serial.println(colorThroughLightStrength);
+  
   int colorCode[] = {255, colorThroughLightStrength, colorThroughLightStrength};
 
-  leftButtonPressed = CircuitPlayground.leftButton();
   rightButtonPressed = CircuitPlayground.rightButton();
 
   if(button_1->retrieveSensorData()) {
