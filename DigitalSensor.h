ebbfa4d5e56587abec35a6954f5bb9d209a4bb65
Lucas Hijman <0968128@hr.nl>
2021-03-20 15:38:25 +0100

    double definition & variable
    inserted standard variable and prevention code for double inclusion

--- DigitalSensor.h
+++ DigitalSensor.h
@@ -1,6 +1,11 @@
+#ifndef DIGITAL_SENSOR
+#define DIGITAL_SENSOR
 #include <Adafruit_CircuitPlayground.h>
 
 class DigitalSensor {
-  public:    
+  public:
+    bool _digitalSensorValue;
     virtual bool retrieveSensorData();
 };
+
+#endif
